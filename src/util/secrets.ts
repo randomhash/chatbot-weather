5574f1db601805a7e87d558d08f18cf5af734221
Andrii Klubuk <61245342+randomhash@users.noreply.github.com>
2021-06-14 10:41:17 +0300

    Update secrets.ts

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -3,13 +3,9 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 import logger from './logger';
 
-if (fs.existsSync('.env')) {
-  logger.debug('Using .env file to supply config environment variables');
-  dotenv.config({path: '.env'});
-} else {
-  logger.debug('Using .env.example file to supply config environment variables');
-  dotenv.config({path: '.env.example'});
-}
+logger.debug('Using .env file to supply config environment variables');
+dotenv.config({path: '.env'});
+
 export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === 'production';
 
